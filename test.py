0ef5e36e7cb2ea0a79034b8f16f771385e61761c
Matt Clay <matt@mystile.com>
2019-10-14 10:56:42 -0700

    Add failure.

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-import time
 
 job = sys.argv[1]
 
-time.sleep(20)
-
 print('Hello %s on %s' % (job, '.'.join('%s' % v for v in sys.version_info)))
+
+if job == 'J003':
+    print('Fail')
+    sys.exit(1)
