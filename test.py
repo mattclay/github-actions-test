e1307e580202d845eede125f5c1bcffa08ba16fe
Matt Clay <matt@mystile.com>
2019-10-31 09:34:28 -0700

    Show environment.

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 job = sys.argv[1]
 
 print('Hello %s on %s' % (job, '.'.join('%s' % v for v in sys.version_info)))
 print('Update 1')
+print('\n'.join('%s=%s' % (key, value) for key, value in os.environ.items()))
