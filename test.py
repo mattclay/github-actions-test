dc0313bc56f3d3123aa2084cbd22d3da47364809
Matt Clay <matt@mystile.com>
2020-05-08 15:09:18 -0700

    Show event data.

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 
 job = sys.argv[1]
 
@@ -12,3 +13,12 @@ print()
 
 for key in sorted(os.environ):
     print('%s=%s' % (key, os.environ[key]))
+
+event_path = os.environ['GITHUB_EVENT_PATH']
+
+with open(event_path, 'r') as event_file:
+    event = json.load(event_file)
+
+print()
+
+print(json.dumps(event, indent=4, sort_keys=True))
