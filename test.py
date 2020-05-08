e1bf86a102fca5599d486fd41d975a6b4f7757ef
Matt Clay <matt@mystile.com>
2020-05-08 15:06:54 -0700

    Show environ sorted.

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 job = sys.argv[1]
 
 print('Hello %s on %s' % (job, '.'.join('%s' % v for v in sys.version_info)))
 print('Update 1')
+
+print()
+
+for key in sorted(os.environ):
+    print('%s=%s' % (key, os.environ[key]))
